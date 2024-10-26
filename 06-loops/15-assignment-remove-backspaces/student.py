b103d488a89caee7127956bbd8f25be1d45815c9
Quinten <r1058961@ucll.be>
2024-10-26 14:41:13 +0200

    upload alle oefeningen van loops af

--- 06-loops/15-assignment-remove-backspaces/student.py
+++ 06-loops/15-assignment-remove-backspaces/student.py
@@ -6,7 +6,7 @@ def remove_backspaces(string):
             if index - 1 < 0:
                 string = ""
             else:
-                string = string[: index] + string[index:]
+                string = string[: index - 1] + string[index + 1:]
                 
         else:
             index += 1
@@ -17,3 +17,4 @@ print(remove_backspaces("\b"))
 print(remove_backspaces("abc"))
 print(remove_backspaces("13\b2356\b\b45677\b8990\b\b0"))
 print(remove_backspaces("abc\b\bxyz")) 
+print(remove_backspaces('1235\b084567899\b080'))
