561a703d9101a4731d28440dc320eebb2ae1d589
Quinten <r1058961@ucll.be>
2024-11-05 15:34:52 +0100

    named tupples verwijderen

--- 07-tuples/11-assignment-heatwave/student.py
+++ 07-tuples/11-assignment-heatwave/student.py
@@ -1,5 +1,14 @@
 # write your code here
+def check_heatwave(temperatures):
+    30days = 0
+    for temp in temperatures:
+        if temp < 25:
+            return False
+        if temp >= 30
+
+
 def heatwave(temperatures):
     if len(temperatures) < 5:
         return False
+    
     
